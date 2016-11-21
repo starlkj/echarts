435ba8e38291875cc4ac8f0d3c4134d00b62c5ba
starlkj <starlkj@exntu.com>
2016-11-21 15:35:53 +0900

    2016-11-21 slieder zoom 디자인 수정

--- src/component/dataZoom/SliderZoomModel.js
+++ src/component/dataZoom/SliderZoomModel.js
@@ -5,6 +5,14 @@ define(function(require) {
 
     var DataZoomModel = require('./DataZoomModel');
 
+    /**
+     * SliderZoomModel 색상 변경
+     * edit by starlkj - #20161121-1
+     *  backgroundColor
+     *  dataBackground
+     *  fillerColor
+     *  textStyle
+     */
     var SliderZoomModel = DataZoomModel.extend({
 
         type: 'dataZoom.slider',
@@ -26,34 +34,39 @@ define(function(require) {
             left: null,   // Default align to grid rect.
             bottom: null, // Default align to grid rect.
 
-            backgroundColor: 'rgba(47,69,84,0)',    // Background of slider zoom component.
+            //backgroundColor: 'rgba(41,123,184,0.2)',    // Background of slider zoom component. - 원본
+            backgroundColor: '#f9f9f9',    // Background of slider zoom component.
             // dataBackgroundColor: '#ddd',         // Background coor of data shadow and border of box,
                                                     // highest priority, remain for compatibility of
                                                     // previous version, but not recommended any more.
             dataBackground: {
                 lineStyle: {
-                    color: '#2f4554',
+                    //color: '#2f4554', 원본
+                    color: '#dcdedf',
                     width: 0.5,
-                    opacity: 0.3
+                    //opacity: 0.3 원본
+                    opacity: 1
                 },
                 areaStyle: {
-                    color: 'rgba(47,69,84,0.3)',
-                    opacity: 0.3
+                    //color: 'rgba(47,69,84,0.3)', 원본
+                	color: '#ebeff1',
+                    opacity: 1
                 }
             },
-            borderColor: '#ddd',                    // border color of the box. For compatibility,
+            borderColor: '#eee',                    // border color of the box. For compatibility,
                                                     // if dataBackgroundColor is set, borderColor
                                                     // is ignored.
 
-            fillerColor: 'rgba(167,183,204,0.4)',     // Color of selected area.
-            // handleColor: 'rgba(89,170,216,0.95)',     // Color of handle.
+            //fillerColor: 'rgba(167,183,204,0.4)',   원본  // Color of selected area.
+            fillerColor: 'rgba(41,123,184,0.2)',
+            // handleColor: 'rgba(89,170,216,0.95)',   원본 // Color of handle.
             // handleIcon: 'path://M4.9,17.8c0-1.4,4.5-10.5,5.5-12.4c0-0.1,0.6-1.1,0.9-1.1c0.4,0,0.9,1,0.9,1.1c1.1,2.2,5.4,11,5.4,12.4v17.8c0,1.5-0.6,2.1-1.3,2.1H6.1c-0.7,0-1.3-0.6-1.3-2.1V17.8z',
             handleIcon: 'M8.2,13.6V3.9H6.3v9.7H3.1v14.9h3.3v9.7h1.8v-9.7h3.3V13.6H8.2z M9.7,24.4H4.8v-1.4h4.9V24.4z M9.7,19.1H4.8v-1.4h4.9V19.1z',
             // Percent of the slider height
             handleSize: '100%',
 
             handleStyle: {
-                color: '#a7b7cc'
+                color: '#297bb8'
             },
 
             labelPrecision: null,
@@ -63,7 +76,7 @@ define(function(require) {
             realtime: true,
             zoomLock: false,                        // Whether disable zoom.
             textStyle: {
-                color: '#333'
+                color: '#444'
             }
         }
 
